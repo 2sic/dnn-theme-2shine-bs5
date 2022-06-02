313842a0a39612534c4a704a928a7341ede59499
Roman Opalko <roman.opalko@2sic.com>
2022-06-02 10:02:40 +0200

    updated npm packages, better webpack performance

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,82 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const WebpackBar = require('webpackbar');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  mode: "production",
   entry: ['./src/scss/theme.scss', './src/ts/theme.ts'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'theme.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]'
   },
-  mode: 'production',
   devtool: 'source-map',
   watch: true,
   stats: {
-    all: false,
-    assets: true
+    warnings: false,
+    cachedModules: false,
+    groupModulesByCacheStatus: false
+  },
+  cache: {
+    type: 'filesystem',
+    cacheDirectory: path.resolve(__dirname, '.temp_cache'),
+    compression: 'gzip',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.scss']
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-      new OptimizeCSSAssetsPlugin({
-        cssProcessorOptions: {
-          map: {
-            inline: false,
-            annotation: true,
-          }
-        }
-      })
-    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'theme.min.css',
     }),
-    new WebpackBar(),
-    new FriendlyErrorsWebpackPlugin(),
+    new webpack.ProgressPlugin(),
   ],
   module: {
-    rules: [{
-        test: /\.scss$/,
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
         exclude: /node_modules/,
+      }, {
+        test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-                sourceMap: true
-            }
+              sourceMap: true
+            },
           }, {
             loader: 'postcss-loader',
             options: {
               sourceMap: true,
               postcssOptions: {
-                plugins: [
-                  require('autoprefixer')
-                ]
+                plugins: function () {
+                  return [
+                    require('autoprefixer')
+                  ];
+                }
               }
             }
           }, {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
-                sourceMap: true
-            }
+              sourceMap: true
+            },
           }
         ],
-      },
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader'
-        }
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'images/'
-          }
-        }]
       }
     ],
   },
 };
+
+new webpack.ProgressPlugin((percentage, message) => {
+  console.log(`${(percentage * 100).toFixed()}% ${message}`);
+})

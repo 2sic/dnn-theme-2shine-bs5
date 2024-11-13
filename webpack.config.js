2b955765dcefa2a73fbbbf9d316bb11ba3090801
Roman Opalko <roman@opalko.de>
2024-11-13 11:28:10 +0100

    wip: npm update, webpack improvement

--- webpack.config.js
+++ webpack.config.js
@@ -2,81 +2,95 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: ['./src/scss/styles.scss', './src/ts/scripts.ts'],
-  output: {
-    filename: 'scripts.min.js',
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash][ext][query]'
-  },
-  devtool: 'source-map',
-  watch: true,
-  stats: {
-    warnings: false,
-    cachedModules: false,
-    groupModulesByCacheStatus: false
-  },
-  cache: {
-    type: 'filesystem',
-    cacheDirectory: path.resolve(__dirname, '.temp_cache'),
-    compression: 'gzip',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.scss']
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'styles.min.css',
-    }),
-    new webpack.ProgressPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
+module.exports = () => {
+  let lastMessage = '';
+
+  return {
+    mode: "development",
+    entry: ['./src/scss/styles.scss', './src/ts/scripts.ts'],
+    output: {
+      filename: 'scripts.min.js',
+      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: 'images/[hash][ext][query]'
+    },
+    devtool: 'source-map',
+    watch: true,
+    cache: {
+      type: 'filesystem',
+      cacheDirectory: path.resolve(__dirname, '.temp_cache'),
+      compression: 'gzip',
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.scss']
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.min.css',
+      }),
+      new webpack.ProgressPlugin((percentage, message) => {
+        const progress = Math.round(percentage * 100);
+        const progressBar = `[${'='.repeat(progress / 2)}${' '.repeat(50 - progress / 2)}]`;
+        if (message !== lastMessage) {
+          console.log(`${progress}% ${progressBar} ${message}`);
+          lastMessage = message;
+        }
+      }),
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },  
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true,
+              },
             },
-          },
-        ],
-        exclude: /node_modules/,
-      }, {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true
-            },
-          }, {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              postcssOptions: {
-                plugins: function () {
-                  return [
-                    require('autoprefixer')
-                  ];
+          ],
+          exclude: /node_modules/,
+        }, 
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              },
+            }, 
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                postcssOptions: {
+                  plugins: function () {
+                    return [
+                      require('autoprefixer')
+                    ];
+                  }
                 }
               }
+            }, 
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+                sassOptions: {
+                  silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
+                }
+              },
             }
-          }, {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true
-            },
-          }
-        ],
-      }
-    ],
-  },
+          ],
+          exclude: /node_modules/,
+        }
+      ],
+    },
+  };
 };
-
-new webpack.ProgressPlugin((percentage, message) => {
-  console.log(`${(percentage * 100).toFixed()}% ${message}`);
-})
